8899d1fb9e98cdcaaaa3fe80916ad65828f6cdbb
sos-git <108962568+sos-git@users.noreply.github.com>
2026-03-10 04:44:00 +0200

    Create controller_baseline.sv
    adding CB logic

---
+++ controller_baseline.sv
@@ -0,0 +1,11 @@
+// controller_baseline logic
+
+module controller_baseline (clk, go, comp_out, valid, sample, result, value);
+
+input clk,go, comp_out;
+output sample,valid;
+output [9:0] result,value;
+
+// logic placeholder
+
+endmodule
